40527bb07427c46d151c40648d41d0f6c88ae195
Jasper Kense <jasperkense@hotmail.com>
2021-03-12 14:02:32 +0100

    Add Revision Page

--- ImageGalleryLoader.js
+++ ImageGalleryLoader.js
@@ -4,10 +4,8 @@ $.ajax({
     url : folder,
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-
-                // $("body").append( "<img src='"+ folder + val +"'>" );
-
+            if( val.match(/\.(jpe?g|JPG|png|gif)$/) ) { 
+                $("body").append( "<img class='img-fluid' alt='100%x280' src='"+ folder + val +"'>");
             } 
         });
     }
