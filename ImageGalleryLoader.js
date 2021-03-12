f5c0a6ab31472be109864f1f3c34bf806cb62a58
Jasper Kense <jasperkense@hotmail.com>
2021-03-12 15:05:48 +0100

    Debug

--- ImageGalleryLoader.js
+++ ImageGalleryLoader.js
@@ -3,6 +3,8 @@ var folder = "/StoreOfTheFutureDemo/Database/";
 $.ajax({
     url : folder,
     success: function (data) {
+        console.log(data);
+
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
                 var _img = document.getElementById('body');
