9072ef4384bb9a596f0e89d2d8b6e1b24a07c2d3
Jasper Kense <jasperkense@hotmail.com>
2021-04-23 14:40:43 +0200

    Edit Voice Visualization

--- AIVoice.js
+++ AIVoice.js
@@ -1,45 +1,44 @@
-function preload(){
-    sound = loadSound('/StoreOfTheFutureDemo/asset/voicebooking-speech.wav');
-  }
-  
-  function setup(){
-    let canvas = createCanvas(300,300);
-    canvas.parent("p5");
-    canvas.mouseClicked(togglePlay);
-    fft = new p5.FFT();
-    sound.amp(0.2);
-  }
-  
-  function draw(){
-    background(0);
-  
-    let spectrum = fft.analyze();
-    noStroke();
-    fill(255, 132, 255);
-    for (let i = 0; i< spectrum.length; i++){
-      let x = map(i, 0, spectrum.length, 0, width);
-      let h = -height + map(spectrum[i], 0, 255, height, 0);
-      rect(x, height, width / spectrum.length, h )
-    }
-  
-    let waveform = fft.waveform();
-    noFill();
-    beginShape();
-    stroke(20);
-    for (let i = 0; i < waveform.length; i++){
-      let x = map(i, 0, waveform.length, 0, width);
-      let y = map( waveform[i], -1, 1, 0, height);
-      vertex(x,y);
-    }
-    endShape();
-  
-    text('tap to play', 20, 20);
-  }
-  
-  function togglePlay() {
-    if (sound.isPlaying()) {
-      sound.pause();
+  let song, buttton, fft, space_between_lines;
+
+  function toggleSong() {
+    if(song.isPlaying()) {
+      song.pause();
     } else {
-      sound.play();
+      song.play();
     }
+  }
+  
+  function preload() {
+    song = loadSound('/StoreOfTheFutureDemo/asset/voicebooking-speech.wav');
+  }
+  
+  function setup() {
+    createCanvas(600, 600);
+    angleMode(DEGREES); // Change the mode to DEGREES
+    colorMode(HSB);
+    buttton = select('#audioPlayer');
+    buttton.mousePressed(toggleSong);
+    song.play();  
+    fft = new p5.FFT(0.9, 128);
+    space_between_lines = width / 128;
+  }
+  
+  //Switch to branch symmetric-spectrum using "git checkout symmetric-spectrum" if you want symmetric spectrum.
+  
+  function draw() {
+    background(0, 0);
+    
+    let spectrum = fft.analyze();
+    for (let i = 0; i < spectrum.length; i++) {
+      fill(i,255,255);
+      let amp = spectrum[i];
+      let y = map(amp, 0, 256, height, 0);
+      rect(i * space_between_lines, y, space_between_lines, height - y);
+    }
+  }
+  
+  // Chrome 70 will require user gestures to enable web audio api
+  // Click on the web page to start audio
+  function touchStarted() {
+    getAudioContext().resume();
   }
