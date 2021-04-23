2249a7c352378e0a391070d004690a79006f3119
Jasper Kense <jasperkense@hotmail.com>
2021-04-23 11:07:40 +0200

    Create link with Bubble Rater

--- AIVoice.js
+++ AIVoice.js
@@ -3,18 +3,18 @@ function preload(){
   }
   
   function setup(){
-    let cnv = createCanvas(100,100);
+    let cnv = createCanvas(300,300);
     cnv.mouseClicked(togglePlay);
     fft = new p5.FFT();
     sound.amp(0.2);
   }
   
   function draw(){
-    background(220);
+    background(0);
   
     let spectrum = fft.analyze();
     noStroke();
-    fill(255, 0, 255);
+    fill(255, 132, 255);
     for (let i = 0; i< spectrum.length; i++){
       let x = map(i, 0, spectrum.length, 0, width);
       let h = -height + map(spectrum[i], 0, 255, height, 0);
